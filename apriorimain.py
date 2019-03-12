e0c15ffdbd7ede2eb294744b9807f6fc7a265f72
jtravis3 <39348376+jtravis3@users.noreply.github.com>
2019-03-11 23:27:23 -0400

    git

--- apriorimain.py
+++ apriorimain.py
@@ -1,1 +1,12 @@
-def 
+from apriori import *
+def runapriori(file,delim,support):
+    x =dataprocess(file, delim).tuplemaker()
+
+    y = apriori(x, support)
+    y.frequent1items()
+    for n in range(1,len(x[0])-1):
+        y.merge(n)
+        y.prune(n+1)
+    print(y.frequents)
+
+runapriori('adult.data.train.csv',',',0.6)
